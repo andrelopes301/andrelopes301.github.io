47a953c8a7742a2c792f1bc977c13e8d48d00a27
andrelopes301 <andrerlopes00@gmail.com>
2022-02-05 01:37:36 +0000

    Updates

---
+++ precache-manifest.d6ff3bda7a6463e29031fcd7cc793374.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6093f842281d504498d7b0a9227bca62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b241d29d5d44835a0b5",
+    "url": "/static/css/main.82458425.chunk.css"
+  },
+  {
+    "revision": "3a33124c704945b257b7",
+    "url": "/static/js/2.ef72e1c4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ef72e1c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b241d29d5d44835a0b5",
+    "url": "/static/js/main.e00f9f99.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "57f4bcb94ef62eca1dd306ef7eb3c192",
+    "url": "/static/media/perfil.57f4bcb9.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
